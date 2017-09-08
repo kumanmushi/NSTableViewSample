4878bcdb190c3f3ba069ed7e90a30c469e4efe44
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-09-08 23:50:05 +0900

    プロジェクトを追加

---
+++ NSTableViewSample/NSTableViewSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTableViewSample
+//
+//  Created by 村田真矢 on 2017/09/08.
+//  Copyright © 2017年 村田真矢. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTableViewSample/NSTableViewSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTableViewSample
+//
+//  Created by 村田真矢 on 2017/09/08.
+//  Copyright © 2017年 村田真矢. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
