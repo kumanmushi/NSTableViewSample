49d3986df39855b572132d1d3d2a0fe64ab8bf6f
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-09-10 12:12:42 +0900

    文字,画像のCellを表示

--- NSTableViewSample/NSTableViewSample/ViewController.swift
+++ NSTableViewSample/NSTableViewSample/ViewController.swift
@@ -8,20 +8,50 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
+    @IBOutlet weak var TableView: NSTableView!
+    
+    private let stringArray: [String] = ["test1", "test2", "test3"]
+    private let imageNameArray: [String] = ["test1", "test2", "test3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    // MARK: - Table View Data Sourse
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.stringArray.count
+    }
+
+
+    // MARK: - Table View Delegate
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        guard let columnIdentifier: String = tableColumn?.identifier else {
+            return nil
         }
+        
+        let stringColumn: String = "String", imageColumn: String = "Image", buttonColumn: String = "Button"
+        switch columnIdentifier {
+        case stringColumn:
+            if let cell = tableView.make(withIdentifier: stringColumn, owner: self) as? NSTableCellView{
+                cell.textField?.stringValue = self.stringArray[row]
+                return cell
+            }
+            
+        case imageColumn:
+            if let cell = tableView.make(withIdentifier: imageColumn, owner: self) as? NSTableCellView{
+                cell.imageView?.image = NSImage(named: self.imageNameArray[row])
+                return cell
+            }
+            
+        case buttonColumn:
+            break
+        
+        default:
+            break
+        }
+        return nil
     }
-
-
 }
-
