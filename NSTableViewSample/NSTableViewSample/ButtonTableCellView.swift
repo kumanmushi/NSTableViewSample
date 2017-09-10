d42f90c000d144cddc9b5766b87c2b97ac9e1cf7
村田真矢 <murata_shinya@ShinyaMurata.local>
2017-09-10 17:57:30 +0900

    ButtonTableCellに関するファイルを作成

---
+++ NSTableViewSample/NSTableViewSample/ButtonTableCellView.swift
@@ -0,0 +1,19 @@
+//
+//  ButtonTableCellView.swift
+//  NSTableViewSample
+//
+//  Created by 村田真矢 on 2017/09/10.
+//  Copyright © 2017年 村田真矢. All rights reserved.
+//
+
+import Cocoa
+
+class ButtonTableCellView: NSTableCellView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
